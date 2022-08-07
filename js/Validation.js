59afa1e987c9abf40b90a6b4e35969a72d8752d7
Son <dohoangson060801@gmail.com>
2022-08-07 21:58:48 +0700

    Validation and search

--- js/Validation.js
+++ js/Validation.js
@@ -1,3 +1,101 @@
 /**
  * This file used to create a new Class Validation that contains all methods for handling validation
  */
+// After finish all this function in main.js , now we move on to validation
+// this array contains all messages to check if user input is empty or not
+var arrayTBEmpty = [
+    "*Mã sinh viên không được để trống", // 0
+    "*Tên sinh viên không được để trống", // 1
+    "*Email sinh viên không được để trống", // 2
+    "*Vui lòng nhập mật khẩu", // 3
+    "*Chọn ngày sinh của bạn", // 4
+    "*Chọn khóa học của bạn", // 5
+    "*Điểm môn học không được để trống" // 6
+];
+// this array contains all messages to check if input field is valid or not
+var arrayValid = [
+    "*Mã sinh viên đã tồn tại" ,
+    "*Tên sinh viên chỉ chứa kí tự chữ cái" , 
+    "*Email chưa đúng định dạng" , 
+    "*Mật khẩu chứa ít nhất 1 số , 1 chữ in hoa , 1 chữ in thường và 1 số , có độ dài từ 6 đến 8 kí tự" , 
+    "*Ngày sinh không hợp lệ" , 
+    "*Điểm không hợp lệ"
+];
+var regexPattern = [
+    /^[a-z A-Z_ÀÁÂÃÈÉÊẾÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶ" + "ẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểếỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợ" + "ụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹý\\s]+$/ , // regex for name
+    /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ , //regex for email
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{6,8}$/ , // regex password
+    /^(\d{1,2}(\.\d{1,2})?)$/ , // regex check score
+
+
+]
+function Validation() {
+    // this method used to check empty
+    this.checkEmpty = function(inputValue , idSpanTB , message){
+        if(inputValue.trim() != ""){
+            getELE(idSpanTB).style.display = "none" ; 
+            getELE(idSpanTB).innerHTML = "" ; 
+            return true ; 
+        }else{
+            getELE(idSpanTB).style.display = "block" ; 
+            getELE(idSpanTB).innerHTML = message ;
+            return false ; 
+        }
+    }
+    // this method used to check khoahocSV empty or not
+    this.checkKhoaHoc = function(idSelectKH , idSpanTB , message){
+        var selectValue = getELE(idSelectKH) ; 
+        if(selectValue.selectedIndex != 0){
+            getELE(idSpanTB).style.display = "none" ; 
+            getELE(idSpanTB).innerHTML = "" ; 
+            return true ; 
+        }else{
+            getELE(idSpanTB).style.display = "block" ; 
+            getELE(idSpanTB).innerHTML = message ;
+            return false ; 
+        }
+    }
+    // this method used to check if maSV is existed or not
+    this.checkMaSVExist = function(maSV , idSpanTB , message){
+        var checkExist = dssv.listSV.some(function(objectSV){
+            if(objectSV.maSV === maSV.replaceAll(" ","")) return true ; 
+        })
+        if(checkExist){
+            getELE(idSpanTB).style.display = "block" ; 
+            getELE(idSpanTB).innerHTML = message ;
+            return false ; 
+        }else{
+            getELE(idSpanTB).style.display = "none" ; 
+            getELE(idSpanTB).innerHTML = "" ; 
+            return true ; 
+
+        }
+    }
+    // this method used to check tenSV using regex
+    this.checkRegex = function(inputValue , patternRegex , idSpanTB , message){
+        var pattern = patternRegex ; 
+        if(inputValue.match(pattern)){
+            getELE(idSpanTB).style.display = "none" ; 
+            getELE(idSpanTB).innerHTML = "" ; 
+            return true ;
+        }else{
+            getELE(idSpanTB).style.display = "block" ; 
+            getELE(idSpanTB).innerHTML = message ;
+            return false ;
+        }
+    }
+    // check score
+    this.checkScore = function(inputValue , idSpanTB , message){
+        var pattern = regexPattern[3] ;
+        if(inputValue.match(pattern) && Number(inputValue) <= 10){
+            getELE(idSpanTB).style.display = "none" ; 
+            getELE(idSpanTB).innerHTML = "" ; 
+            return true ;
+        }else{
+            getELE(idSpanTB).style.display = "block" ; 
+            getELE(idSpanTB).innerHTML = message ;
+            return false ;
+        }  
+    }
+ 
+}
--- js/main.js
+++ js/main.js
@@ -19,18 +19,44 @@ function addSV() {
     var diemToan = getELE("txtDiemToan").value ;
     var diemLy = getELE("txtDiemLy").value ;
     var diemHoa = getELE("txtDiemHoa").value;
-    // now we creat a new object SinhVien then push in listSV 
-    var svObject = new SinhVien(maSV , tenSV , emailSV , passwordSV , ngaysinhSV , khoaHocSV , Number(diemToan) , Number(diemLy)  , Number(diemHoa) ) ; 
+    // before we create a new object SV , we need to check user input is valid or not
+    var validation = new Validation() ; // we create this instance to use method that we wrote in Validation file
+    var isValid = true; // we create a variabe 'isValid' and assgin it value of true
+    // check maSV
+    isValid &= validation.checkEmpty(maSV , "spanMaSV" , arrayTBEmpty[0]) && validation.checkMaSVExist(maSV , "spanMaSV" , arrayValid[0]) ; 
+    // check tenSV
+    isValid &= validation.checkEmpty(tenSV , "spanTenSV" , arrayTBEmpty[1]) && validation.checkRegex(tenSV , regexPattern[0] , "spanTenSV" , arrayValid[1]) ; 
+    // check emailSV
+    isValid &= validation.checkEmpty(emailSV , "spanEmailSV" , arrayTBEmpty[2]) && validation.checkRegex(emailSV ,  regexPattern[1] , "spanEmailSV" , arrayValid[2]) ; 
+    // check passwordSV
+    isValid &= validation.checkEmpty(passwordSV , "spanMatKhau" , arrayTBEmpty[3]) && validation.checkRegex(passwordSV ,  regexPattern[2] , "spanMatKhau" , arrayValid[3])  ; 
+    // check ngaysinhSV
+    isValid &= validation.checkEmpty(ngaysinhSV , "spanNgaySinh" , arrayTBEmpty[4]) ;
+    // check khoahocSV
+    isValid &= validation.checkKhoaHoc("khSV" , "spanKhoaHoc" , arrayTBEmpty[5]) ;
+    // check diemToan
+    isValid &= validation.checkEmpty(diemToan , "spanToan" , arrayTBEmpty[6] ) && validation.checkScore(diemToan , "spanToan" , arrayValid[5]) ;
+    isValid &= validation.checkEmpty(diemLy , "spanLy" , arrayTBEmpty[6] ) && validation.checkScore(diemLy , "spanLy" , arrayValid[5]) ;
+    isValid &= validation.checkEmpty(diemHoa , "spanHoa" , arrayTBEmpty[6] ) && validation.checkScore(diemHoa , "spanHoa" , arrayValid[5]) ;
+    console.log(isValid) ; 
+    
+    
+
+    if(isValid){
+        // now we creat a new object SinhVien then push in listSV 
+        var svObject = new SinhVien(maSV , tenSV , emailSV , passwordSV , ngaysinhSV , khoaHocSV , Number(diemToan) , Number(diemLy)  , Number(diemHoa) ) ; 
     // console.log(svObject) ; 
     // now we push it in array listSV
-    dssv.addSV(svObject) ; 
+        dssv.addSV(svObject) ; 
     // console.log(dssv.listSV) ; 
     // after we push it in array , we need to show it on UI so users can see it
-    showUI(dssv.listSV) ; 
+        showUI(dssv.listSV) ; 
     // when we reload the page , all datas will disapear
     // in this pratice , we dont know API yet , so we will store it in local storage
-    setLocalStorage(dssv.listSV) ; 
+        setLocalStorage(dssv.listSV) ; 
 
+    }
+    
 
 
 
@@ -117,7 +143,28 @@ function updateSV() {
     var diemToan = getELE("txtDiemToan").value ;
     var diemLy = getELE("txtDiemLy").value ;
     var diemHoa = getELE("txtDiemHoa").value;
-    var objectSV=  new SinhVien(maSV , tenSV , emailSV , passwordSV , ngaysinhSV , khoaHocSV , Number(diemToan) , Number(diemLy) , Number(diemHoa)) ; 
+    // we need to check validation first then update data
+
+    var validation = new Validation() ; 
+    var isValid = true ; 
+     
+    // check tenSV
+    isValid &= validation.checkEmpty(tenSV , "spanTenSV" , arrayTBEmpty[1]) && validation.checkRegex(tenSV , regexPattern[0] , "spanTenSV" , arrayValid[1]) ; 
+    // check emailSV
+    isValid &= validation.checkEmpty(emailSV , "spanEmailSV" , arrayTBEmpty[2]) && validation.checkRegex(emailSV ,  regexPattern[1] , "spanEmailSV" , arrayValid[2]) ; 
+    // check passwordSV
+    isValid &= validation.checkEmpty(passwordSV , "spanMatKhau" , arrayTBEmpty[3]) && validation.checkRegex(passwordSV ,  regexPattern[2] , "spanMatKhau" , arrayValid[3])  ; 
+    // check ngaysinhSV
+    isValid &= validation.checkEmpty(ngaysinhSV , "spanNgaySinh" , arrayTBEmpty[4]) ;
+    // check khoahocSV
+    isValid &= validation.checkKhoaHoc("khSV" , "spanKhoaHoc" , arrayTBEmpty[5]) ;
+    // check diemToan
+    isValid &= validation.checkEmpty(diemToan , "spanToan" , arrayTBEmpty[6] ) && validation.checkScore(diemToan , "spanToan" , arrayValid[5]) ;
+    isValid &= validation.checkEmpty(diemLy , "spanLy" , arrayTBEmpty[6] ) && validation.checkScore(diemLy , "spanLy" , arrayValid[5]) ;
+    isValid &= validation.checkEmpty(diemHoa , "spanHoa" , arrayTBEmpty[6] ) && validation.checkScore(diemHoa , "spanHoa" , arrayValid[5]) ;
+    console.log(isValid) ;
+    if(isValid){
+        var objectSV=  new SinhVien(maSV , tenSV , emailSV , passwordSV , ngaysinhSV , khoaHocSV , Number(diemToan) , Number(diemLy) , Number(diemHoa)) ; 
     var index = dssv.findSV(maSV) ; 
     if(index > - 1){
         console.log(index) ; 
@@ -129,7 +176,8 @@ function updateSV() {
     // after update then show UI and set local
     showUI(dssv.listSV) ; 
     setLocalStorage(dssv.listSV) ;
-    formReset() ;  
+    formReset() ;
+    }  
     
 }
 getELE("btnUpdate").onclick = updateSV ; 
@@ -137,4 +185,50 @@ getELE("btnUpdate").onclick = updateSV ;
 function formReset() {
     getELE("formQLSV").reset() ;
     getELE("txtMaSV").disabled = false ; 
+    showUI(dssv.listSV) ; 
 }
+getELE("btnReset").addEventListener("click" , formReset) ; 
+// search method
+function searchSV() {
+    // create an array to save all student we want to look for
+    var searchArray = [] ; // we will assign the value of empty for array at first
+    // now we need to get the value that users will type in form input
+    var searchKey = getELE("txtSearch").value ; 
+    // after we get the keyword , we need to convert it to lowerCase and noneAccentVietnamese
+    searchKey = toNonAccentVietnamese(searchKey).toLowerCase() ; 
+    dssv.listSV.map(function(objectSV){
+        var tenSV = toNonAccentVietnamese(objectSV.tenSV).toLowerCase() ; 
+        if(tenSV.indexOf(searchKey) > -1) searchArray.push(objectSV) ; 
+    })
+    console.log(searchArray) ;
+    // oke cool now we get all elements we search for
+    // next thing i wanna do is i will show it on UI
+    if(searchArray.length != 0) showUI(searchArray) ;
+    else getELE("tbodySinhVien").innerHTML = `<h2 class = "text-primary my-4">No Student Found !</h2>` ; 
+     
+
+}
+// getELE("btnSearch").addEventListener("click" , searchSV) ; 
+getELE("txtSearch").onkeyup = searchSV ; 
+// hàm này sẽ loại bỏ dấu của tiếng việt
+function toNonAccentVietnamese(str) {
+    str = str.replace(/A|Á|À|Ã|Ạ|Â|Ấ|Ầ|Ẫ|Ậ|Ă|Ắ|Ằ|Ẵ|Ặ/g, "A");
+    str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+    str = str.replace(/E|É|È|Ẽ|Ẹ|Ê|Ế|Ề|Ễ|Ệ/, "E");
+    str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+    str = str.replace(/I|Í|Ì|Ĩ|Ị/g, "I");
+    str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+    str = str.replace(/O|Ó|Ò|Õ|Ọ|Ô|Ố|Ồ|Ỗ|Ộ|Ơ|Ớ|Ờ|Ỡ|Ợ/g, "O");
+    str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+    str = str.replace(/U|Ú|Ù|Ũ|Ụ|Ư|Ứ|Ừ|Ữ|Ự/g, "U");
+    str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+    str = str.replace(/Y|Ý|Ỳ|Ỹ|Ỵ/g, "Y");
+    str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+    str = str.replace(/Đ/g, "D");
+    str = str.replace(/đ/g, "d");
+    // Some system encode vietnamese combining accent as individual utf-8 characters
+    str = str.replace(/\u0300|\u0301|\u0303|\u0309|\u0323/g, ""); // Huyền sắc hỏi ngã nặng 
+    str = str.replace(/\u02C6|\u0306|\u031B/g, ""); // Â, Ê, Ă, Ơ, Ư
+    return str;
+}
+
